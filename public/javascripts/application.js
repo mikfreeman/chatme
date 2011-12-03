e75d2f0aacad8e2fe4011549a44db3ce6a2a9996
Michael Freeman <mikfreeman@gmail.com>
2011-12-03 21:07:49 +0000

    Scroll to bottom

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,15 +4,21 @@
  {
  	var message = $('#themessage').val();
  	$('#messages').append("<div class='alert-message block-message success'><p>" + message + "</p></div>");
+ 	scrollToBottom();
 	$('#themessage').val('');	 	
  	socket.emit('newMessage', { message:message });
- 	
-
  }
  
  function recievedMessage(data)
  {	
  	$('#messages').append("<div class='alert-message block-message info'><p>" + data.message + "</p></div>");
+ 	scrollToBottom();
+ }
+ 
+ function scrollToBottom()
+ {
+ 	$("#messages").scrollTop($("#messages")[0].scrollHeight);
+
  }
 
 $(document).ready(function(){
