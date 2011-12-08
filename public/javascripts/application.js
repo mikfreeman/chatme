51c01b4a270bd0f1b9404b1d37fc3e112a1ce7fe
Michael Freeman <mikfreeman@gmail.com>
2011-12-08 20:35:31 +0000

    Grrr

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -23,13 +23,6 @@
 
 $(document).ready(function(){
 
-if (window.WebSocket) {
-  alert("WebSockets is supported in your browser.");
-} else {
-  alert("WebSockets is NOT supported in your browser.");
-}
-
-
 	$('#sendButton').click(sendMessage);
 
 	socket = io.connect('http://localhost:80',{transports:['websocket']});
